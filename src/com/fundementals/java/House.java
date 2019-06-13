6d3aac37b8d266c35c6176e2317d51121ed2a0b0
genetakinaw1 <genet.akinaw1@gmail.com>
2019-06-13 16:03:02 -0500

    Lesson 4: My  Application overview

---
+++ src/com/fundementals/java/House.java
@@ -0,0 +1,16 @@
+package com.fundementals.java;
+/*
+* This is our first class that we create ourselves
+* */
+public class House {
+
+    String roofType;
+    int windowSize;
+    String fooundation;
+    String doorColor;
+
+    //Our first method (function)
+    public void doorOpenClose(){
+        System.out.println("Open my door");
+    }
+}
--- src/com/fundementals/java/Main.java
+++ src/com/fundementals/java/Main.java
@@ -1,9 +1,15 @@
 package com.fundementals.java;
-
+/*
+*This class is the starting class of this project
+* This is an example of a multiple comment.
+ */
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
    System.out.println("Welcome to Java");
+   House myHouse = new House();
+   myHouse.doorOpenClose();
+
     }
 }
