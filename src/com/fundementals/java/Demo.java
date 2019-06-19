210d49089f9f5ac09eaf4d7ef74cb231044365bf
genetakinaw1 <genet.akinaw1@gmail.com>
2019-06-19 15:50:28 -0500

    Demo

--- src/com/fundementals/java/Demo.java
+++ src/com/fundementals/java/Demo.java
@@ -7,5 +7,53 @@ public class Demo {
         int i=5;
         //displaying the variable
         System.out.println("The value of i is"+i);
+        i=i+1;
+        System.out.println("The value of i is"+1);
+
     }
-}
+      public static void moreDemo() {
+
+
+
+          short i = 5;
+          short j = -6;
+          System.out.println("The value of i is" + i);
+          System.out.println("The value of j is" + j);
+      }
+      public static void moreDemo1(){
+        long k = 10000000;
+        long j = -45000000;
+        System.out.println("The value of i is" +k);
+        System.out.println("The value of j is "+j);
+
+
+
+        float s = 10.11F;
+        float z = -45.44F;
+        System.out.println("The value of s is "+s);
+        System.out.println("The value of j is "+z);
+
+        double m = 10.11111111;
+        double n = -45.4444444;
+        System.out.println("The value of i is "+m);
+        System.out.println("The value of n is "+n);
+
+
+              int x =3;
+              int c =4;
+              System.out.println("The sum of the numbers is "+(x+c));
+
+      }
+
+
+      }
+
+
+
+
+
+
+
+
+
+
